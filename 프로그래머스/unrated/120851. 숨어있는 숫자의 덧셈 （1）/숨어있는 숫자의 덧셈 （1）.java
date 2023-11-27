02356f041309856066f90e1050c9454d9112d440
BPdog <76427395+BPdog@users.noreply.github.com>
2023-11-27 15:04:08 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 1.31 ms, Memory: 69 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public int solution(String my_string) {
+        
+        return Arrays.stream(my_string.replaceAll("[A-Z|a-z]", "").split(""))
+            .mapToInt(Integer::parseInt)
+            .reduce(0, Integer::sum);
+    }
+}
