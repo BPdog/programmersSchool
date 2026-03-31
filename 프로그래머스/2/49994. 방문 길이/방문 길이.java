f6a8c045c367c5c919109cafadd24ee6ff69039f
안영진 <76427395+BPdog@users.noreply.github.com>
2026-03-31 15:23:21 +0900

    [level 2] Title: 방문 길이, Time: 20.49 ms, Memory: 93.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    // 좌표평면을 벗어나는지 체크하는 메소드
+    private static boolean isValidMove(int nx, int ny) {
+        return 0 <= nx && nx < 11 && 0 <= ny && ny < 11;
+    }
+    // 다음 좌표 결정을 위한 HashMap 생성
+    private static final HashMap<Character, int[]> location = new HashMap<>();
+
+    private static void initLocation() {
+        location.put('U', new int[]{0, 1});
+        location.put('D', new int[]{0, -1});
+        location.put('L', new int[]{-1, 0});
+        location.put('R', new int[]{1, 0});
+    }
+    public int solution(String dirs) {
+        initLocation();
+        // 원점에서 출발해 경로의 길이를 구하는 건데, 원점을 (5,5)로 변경하여 음수 좌표 문제를 해결
+        int x = 5, y = 5;
+        HashSet<String> answer = new HashSet<>(); // 겹치는 좌표는 1개로 처리하기 위함
+        // 주어진 명령어로 움직이면서 좌표 저장
+        for (int i = 0; i < dirs.length(); i++) {
+            int[] offset = location.get(dirs.charAt(i));
+            int nx = x + offset[0];
+            int ny = y + offset[1];
+            if (!isValidMove(nx, ny)) // 벗어난 좌표는 인정하지 않음
+                continue;
+            // A에서 B로 간 경우 B에서 A도 추가해야 함(총 경로의 개수는 방향성이 없음)
+            answer.add(x + " " + y + " " + nx + " " + ny);
+            answer.add(nx + " " + ny + " " + x + " " + y);
+            // 좌표를 이동했으므로 업데이트
+            x = nx;
+            y = ny;
+        }
+        // 방향성이 없어 2가지 경우를 넣었음으로 최종 이동 길이를 2로 나눔
+        return answer.size() / 2;
+    }
+}
